50601bbf31b2f352012dce55c1d7a564cf85ee68
Gustavo <gust4v0309@gmail.com>
2023-01-09 22:53:35 -0300

    Feat: App.Vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
-    
+  <div id="app">
+    <TheHeader />
+    <router-view />
   </div>
 </template>
 
 <script>
-export default {
+import TheHeader from './components/TheHeader.vue';
 
+export default {
+  components: {
+    TheHeader
+  }
 }
 </script>
 
 <style>
+  body {
+    margin: 0px;
+    font-family: "Avenir", Arial, Helvetica, sans-serif;
+    color: #234;
+  }
 
+  ul {
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
+  }
+
+  a {
+    font-weight: bold;
+    text-decoration: none;
+    color: #234;
+  }
+
+  a.router-link-exact-active {
+    color: #4B8;
+  }
 </style>
