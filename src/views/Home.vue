cf1f3ccf7699f35a2b8374c968d8840bbe7c419b
Gustavo <gust4v0309@gmail.com>
2023-01-09 22:54:04 -0300

    Feat: Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <h1>Home</h1>
+    </div>
+</template>
+
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+export default {
+    name: "home"
+}
+</script>
+
+<style>
+
+</style>
