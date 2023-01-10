400c228465d528274ab9736b4e8f9dc200467cbe
Gustavo <gust4v0309@gmail.com>
2023-01-09 22:54:00 -0300

    Feat: Cursos.vue

---
+++ src/views/Cursos.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <h1>Cursos</h1>
+    </div>
+</template>
+
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+export default {
+    name: "cursos",
+}
+</script>
+
+<style>
+
+</style>
