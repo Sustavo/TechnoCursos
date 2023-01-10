c29e6c8d2631b4068dbf51adf6a148e888bcc849
Gustavo <gust4v0309@gmail.com>
2023-01-10 20:53:40 -0300

    feat: Contato.vue

--- src/views/Contato.vue
+++ src/views/Contato.vue
@@ -1,14 +1,42 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div>
-        <h1>Contato</h1>
+        <div v-if="loading">
+            <PageLoading />
+        </div>
+        <transition>
+            <div v-if="api">
+                <h1>{{ api.titulo }}</h1>
+                <p>{{ api.descricao }}</p>
+                <ul>
+                    <li>
+                        <b>Email: </b>
+                        {{ api.contato.email }}
+                    </li>
+                    <li>
+                        <b>Telefone: </b>
+                        {{ api.contato.telefone }}
+                    </li>
+                    <li>
+                        <b>Endereco: </b>
+                        {{ api.contato.endereco }}
+                    </li>
+                </ul>
+            </div>
+        </transition>
     </div>
 </template>
 
 <!-- eslint-disable vue/multi-word-component-names -->
 <script>
+import fetchData from '@/mixins/fetchData'
+
 export default {
-    name: "contato"
+    name: "contato",
+    mixins: [fetchData],
+    created() {
+        this.fetchData('/contato');
+    }
 }
 </script>
 
