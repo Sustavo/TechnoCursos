f6a25a2d8f40d8b6e397759a947ffc0f3aee6727
Gustavo <gust4v0309@gmail.com>
2023-01-10 20:53:49 -0300

    feat: Cursos.vue

--- src/views/Cursos.vue
+++ src/views/Cursos.vue
@@ -1,17 +1,43 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div>
-        <h1>Cursos</h1>
+        <div v-if="loading">
+            <PageLoading />
+        </div>
+        <transition>
+            <div v-if="api">
+                <h1>{{ api.titulo }}</h1>
+                <p>{{ api.descricao }}</p>
+                <ul class="cursos-lista">
+                    <li v-for="curso in api.cursos" :key="curso.id">
+                        <h2>
+                            <router-link :to="{name: 'curso', params: {curso: curso.id}}">
+                                {{ curso.nome }} - {{ curso.totalAulas }} aulas | {{ curso.horas }} horas
+                            </router-link>
+                        </h2>
+                        <p>{{ curso.descricao }}</p>
+                    </li>
+                </ul>
+            </div>
+        </transition>
     </div>
 </template>
 
 <!-- eslint-disable vue/multi-word-component-names -->
 <script>
+import fetchData from '@/mixins/fetchData';
+
 export default {
     name: "cursos",
+    mixins: [fetchData],
+    created() {
+        this.fetchData('/cursos');
+    }
 }
 </script>
 
 <style>
-
+.cursos-lista li {
+    margin-bottom: 40px;
+}
 </style>
